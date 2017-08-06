8b9d0965c1be1b8583249d9d96fb30e780508a0c
Yuri A. Parsons <yuri@ninjakitty.net>
2017-08-06 08:42:02 +0200

    open links

--- src/js/components/ListItem.react.js
+++ src/js/components/ListItem.react.js
@@ -3,15 +3,18 @@ import { Button, Icon } from 'semantic-ui-react'
 import { Image } from 'semantic-ui-react'
 import { removeItem } from '../storage'
 
-const CloseButton = (data) => {
+const CloseButton = data => {
   const { favIconUrl, id } = data
   // console.log('data', data)
   const removeThis = removeItem(id)
   return (
-    <Button icon onClick={removeThis} circular size='mini' compact basic>
-      <Icon name="remove" color='red' />
-    </Button>)
+    <Button icon onClick={removeThis} circular size="mini" compact basic>
+      <Icon name="remove" color="red" />
+    </Button>
+  )
 }
+
+const openLink = url => () => chrome.tabs.create({ url: url, active: false })
 
 export default class extends React.Component {
   constructor(props) {
@@ -20,10 +23,11 @@ export default class extends React.Component {
   }
   render() {
     const { url, favIconUrl, title } = this.props
-    //
-    // console.log('LI', this.props)
+    const openThis = openLink(url)
+
+    openLink(url)
     return (
-      <div style={{ marginBottom: '10px' }}>
+      <div style={{ marginBottom: '10px', cursor: 'pointer' }} onClick={openThis}>
         <CloseButton {...this.props} />
         <Image src={favIconUrl} height={14} inline style={{ margin: '0 8px' }} />
         {title}
@@ -34,7 +38,3 @@ export default class extends React.Component {
     )
   }
 }
-/*
-        <Image src={favIconUrl} alt={url} size='mini' />
-
-*/
