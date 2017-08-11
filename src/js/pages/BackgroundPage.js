3a1ec25c676ab55878db622f4282b205358ea3c0
Yuri Parsons <yuri.parsons@deliveryhero.com>
2017-08-11 15:37:52 +0200

    make lists

--- src/js/pages/BackgroundPage.js
+++ src/js/pages/BackgroundPage.js
@@ -1,34 +1,57 @@
 import React from 'react'
 import icon from '../../img/icon-128.png'
 import List from '../components/List.react'
-import { getTabs } from '../storage'
+import  * as Chrome from '../storage'
+import { Button, Icon } from 'semantic-ui-react'
+import { saveTabs, removeItem, openLink } from '../storage'
 
 export default class extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { items: [] }
+    this.state = { items: [], list: [1,2,3] }
   }
 
   componentWillMount() {
-    getTabs('yuri').then(items => {
+    Chrome.getTabs('yuri').then(items => {
       console.info('componentWillMount', items)
-      this.setState({ items: items })
+      this.setState({ counter: 0, lists: [] })
     })
   }
 
   componentDidMount() {
     chrome.storage.onChanged.addListener(() => {
-      getTabs('yuri').then(items => this.setState({ items: items }))
+      Chrome.getTabs('yuri').then(items => this.setState({ items: items }))
     })
   }
 
   render() {
     console.info('background page render')
+    const {lists} = this.state
 
     return (
       <div>
-        Page
-        <List urls={this.state.items} />
+
+    <Button
+        icon
+        size="mini"
+        onClick={() => {
+
+          Chrome.getTabGroups().then( items =>{
+             this.setState({ counter: this.state.counter +1, lists: Object.entries(items) })
+          })
+        }}
+      >
+        <Icon name="home" />Save All Tabs
+      </Button>
+        Counter: {this.state.counter}
+
+        {lists.map( (item) =>
+          <div>
+            things  {item[0]}
+
+          </div> )
+        }
+
       </div>
     )
   }
