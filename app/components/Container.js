21a125be6ba071691c976dfcc534811854765bf7
Yuri A. Parsons <yuri@ninjakitty.net>
2017-09-10 17:30:58 +0200

    remove center container

--- app/components/Container.js
+++ app/components/Container.js
@@ -1,11 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Container } from 'semantic-ui-react'
 import List from '../components/List'
 
-const ContainerPage = (props) => {
-  const { sites } = props
-  return <Container>{sites.map(group => <List key={group.name} group={group} />)}</Container>
-}
+const ContainerPage = ({ sites }) => <div>{sites.map(group => <List key={group.name} group={group} />)}</div>
 
 export default connect(state => ({ options: state.options }))(ContainerPage)
