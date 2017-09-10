fadac6c77b9b382e5299eece6c97c2fc0d77905f
Yuri A. Parsons <yuri@ninjakitty.net>
2017-09-10 13:57:50 +0200

    sections

--- app/containers/Options.js
+++ app/containers/Options.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
+import { Container, Header, Segment, Button, Divider } from 'semantic-ui-react'
 
 export default class OptionsPage extends Component {
   render() {
     const { store } = this.props
     return (
       <Provider store={store}>
-        <div>Options Page</div>
+        <Container>
+          <Header as="h2">Options Page</Header>
+          <Segment padded>Options 1</Segment>
+          <Segment padded>Options 2</Segment>
+          <Segment padded>Options 3</Segment>
+        </Container>
       </Provider>
     )
   }
